baca1bd20b82ac1902551824d8194270942d649a
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2016-08-31 08:30:09 -0400

    changed command prefix to "+"

--- botmodules/age.py
+++ botmodules/age.py
@@ -4,5 +4,5 @@ def age(self, e):
     self.irccontext.privmsg("Angstserv", "deprotect {} {}".format(e.source, e.nick))
     time.sleep(1)
     self.irccontext.mode(e.source, '+b {}'.format(e.hostmask))
-    self.irccontext.kick(e.source, e.nick, "Congratulations! You found the word of the day, courtesy of !age!")
-age.command = "!age"
+    self.irccontext.kick(e.source, e.nick, "Congratulations! You found the word of the day, courtesy of +age!")
+age.command = "+age"
