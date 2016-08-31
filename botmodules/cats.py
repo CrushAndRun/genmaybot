42d71eec1868e6cc2f328cf574ffcb2b11915a31
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2016-08-31 08:49:38 -0400

    changed command prefixs to "+"

--- botmodules/cats.py
+++ botmodules/cats.py
@@ -2,36 +2,36 @@ import json, urllib.request, urllib.error, urllib.parse, random
 def get_coolmaster(self, e):
     e.output = "http://i.imgur.com/NhYEMRi.png"
     return e
-get_coolmaster.command = "!cool"
+get_coolmaster.command = "+cool"
 
 def get_cat(self, e):
     url = "http://www.reddit.com/r/catpictures+cats/.json"
     e.output = get_redditpics(url)
     return e
-get_cat.command = "!cats"
+get_cat.command = "+cats"
 
 def get_mixomatosys(self, e):
     e.output ="i'm not sure hwat you're point is"
     return e
-get_mixomatosys.command = "!mixy"
+get_mixomatosys.command = "+mixy"
 
 
 def get_jeffers(self, e):
     e.output = "http://i.imgur.com/kalVBHv.jpg"
     return e
-get_jeffers.command = "!jeffers"
+get_jeffers.command = "+jeffers"
 
 
 def get_dvq(self, e):
     e.output = "http://i.imgur.com/1lq54.jpg"
     return e
-get_dvq.command = "!dvq"
+get_dvq.command = "+dvq"
 
 def get_rat(self, e):
     url = "http://www.reddit.com/r/rats/.json"
     e.output = get_redditpics(url)
     return e
-get_rat.command = "!rats"
+get_rat.command = "+rats"
 
 def get_rip(self, e):
     e.output = "  ____  ___ ____  \n"
@@ -41,37 +41,37 @@ def get_rip(self, e):
     e.output += " |_| \_\___|_|    \n"
     e.output += "                  \n"
     return e
-get_rip.command = "!rip"
+get_rip.command = "+rip"
 
 def get_dog(self, e):
     url = "http://www.reddit.com/r/dogpictures+dogs/.json"
     e.output = get_redditpics(url)
     return e
-get_dog.command = "!dogs"
+get_dog.command = "+dogs"
 
 def get_bird(self, e):
     url = "http://www.reddit.com/r/birdpics/.json"
     e.output = get_redditpics(url)
     return e
-get_bird.command = "!birds"
+get_bird.command = "+birds"
 
 def get_cacti(self, e):
     url = "http://www.reddit.com/r/cacti/.json"
     e.output = get_redditpics(url)
     return e
-get_cacti.command = "!cacti"
+get_cacti.command = "+cacti"
 
 def get_sloth(self, e):
     url = "http://www.reddit.com/r/sloths/.json"
     e.output = get_redditpics(url)
     return e
-get_sloth.command = "!sloths"
+get_sloth.command = "+sloths"
 
 def get_sandwich(self, e):
     url = "http://www.reddit.com/r/eatsandwiches/.json"
     e.output = get_redditpics(url)
     return e
-get_sandwich.command = "!sandwiches"
+get_sandwich.command = "+sandwiches"
 
 def get_rpics(self, e):
     if not e.input:
@@ -82,7 +82,7 @@ def get_rpics(self, e):
     url = "http://www.reddit.com/r/%s/.json" % e.input
     e.output = get_redditpics(url)
     return e
-get_rpics.command = "!rpics"
+get_rpics.command = "+rpics"
 
 def get_redditpics(url):
     opener = urllib.request.build_opener()
