741dbe37c29743e59e8ee68d98f4663dd96c4b6e
s0ckpuppet <s0ckpuppet@users.noreply.github.com>
2016-08-31 09:20:34 -0400

    changed command prefix to "+"

--- botmodules/kick.py
+++ botmodules/kick.py
@@ -1,3 +1,3 @@
 def do_kick(self, e):
-    self.irccontext.kick("#auzland", e.input, "thank you for participating in this test (by mixomatosys)")
-do_kick.command = "!kick"
+    self.irccontext.kick("#auzland", e.input, "thank you for participating in this test (by lulzytypes)")
+do_kick.command = "+kick"
